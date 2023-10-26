01629e44e2f00e76a7b5c0a1f5cbc7bb0a94064a
maheshgomathi <149047825+maheshgomathi@users.noreply.github.com>
2023-10-26 14:22:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<html>
+<body>
+script type="text/javascript"
+var btn=document.getElementById("btn");
+var light=document.getElementById("light");
+function toggleBtn(){
+btn.classList.toggle("active");
+light.classList.toggle("on");
+}
+</script>
+</body>
+</html>
